c21195b5b087c13fcfc166d04db25e2864999e44
Froezt <36000801+Froezt@users.noreply.github.com>
2022-04-12 23:34:10 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -22,8 +22,11 @@ var diagonal2 = [];
 var rolled = [];
 var stepB = 0;
 var valseBingo = [];
+var dragging = false;
 
+document.getElementById("startbutton").addEventListener("click", start);
 function start() {
+    document.getElementById('startbutton').style.display = "none";
     for (let i = 0; i < 5; i++) {
         const randomNummer = valuesB[Math.floor(Math.random() * valuesB.length)];
         element = document.querySelectorAll(".box");
@@ -163,16 +166,149 @@ function start() {
     console.log(rowO);
     console.log(diagonal1);
     console.log(diagonal2);
-    document.getElementsByClassName('stamp')[0].setAttribute('draggable', true);
+    const stamp = document.querySelector(".list");
+    const kaart = document.querySelector(".kaart");
+    var sortable1 = new Sortable(stamp, {
+        group: {
+            name: 'bingokaart',
+            pull: 'clone'
+        },
+        animation: 150,
+        ghostClass: "sortable-ghost", // Class name for the drop placeholder
+        chosenClass: "sortable-chosen", // Class name for the chosen item
+        dragClass: "sortable-drag",
+        forceFallback: true,
+        onStart: function (evt) {
+            document.querySelector(".kaart").classList.add("dragging");
+            document.querySelector(".box").classList.add("dragging");
+            document.body.classList.add("dragging");
+            dragging = true;
+        },
+        onEnd: function (evt) {
+            document.body.classList.remove("dragging");
+            dragging = false;
+            document.querySelector(".kaart").classList.remove("dragging");
+            document.querySelector(".box").classList.remove("dragging");
+        },
+        scroll: false,
+    });
+
+    document.querySelector(".list li").style.display = "none";
+    /* var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+     var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+     var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+
+     var commandos = ['Bingo', 'bingo'];
+     var grammar = '#JSGF V1.0; grammar commandos; public <commando> = ' + commandos.join(' | ') + ' ;'
+
+
+     var recognition = new SpeechRecognition();
+     var speechRecognitionList = new SpeechGrammarList();
+
+
+     function spraakAfhandelen(event) {
+         let last = event.results.length - 1;
+         let commando = event.results[last][0].transcript;
+         console.log('Result received: ' + commando + '. ' + 'Confidence: ' + event.results[0][0].confidence);
+
+         if (commando.trim() == "bingo" || commando.trim() == "Bingo") {
+             bingoKnop()
+         }
+     }
+
+     function luisteren() {
+         recognition.start();
+         console.log('Ready to receive a command.');
+     }
+
+     speechRecognitionList.addFromString(grammar, 1);
+     recognition.grammars = speechRecognitionList;
+     recognition.continuous = true;
+     recognition.lang = 'en';
+     recognition.interimResults = true;
+     recognition.maxAlternatives = 1;
+
+     recognition.onresult = function (event) {
+         spraakAfhandelen(event);
+     }
+
+     recognition.onend = function () {
+         luisteren();
+     }
+
+     luisteren(); */
 }
 
+
+const nummerBox = document.querySelectorAll(".box");
+
+nummerBox.forEach(nummer => {
+    nummer.addEventListener("mouseover", hoverGetal);
+});
+
+nummerBox.forEach(nummer => {
+    nummer.addEventListener("mouseleave", unhoverGetal);
+});
+
+nummerBox.forEach(nummer => {
+    nummer.addEventListener("mouseup", check);
+});
+
+function hoverGetal(e) {
+    if (dragging && !this.classList.contains("hovered") && !this.classList.contains("checked")) {
+        this.classList.add("hovered");
+    }
+    if (dragging == false && this.classList.contains("checked")) {
+        nummerBox.forEach(nummer => {
+            nummer.addEventListener("click", removeCheck);
+        });
+    }
+}
+
+function unhoverGetal(e) {
+    if (dragging && !this.classList.contains("checked")) {
+        this.classList.remove("hovered");
+    }
+}
+
+function check() {
+    if (dragging == true) {
+        this.classList.add("checked");
+        this.classList.remove("hovered");
+        console.log("gelukt");
+    }
+}
+
+function removeCheck() {
+    this.classList.remove("checked");
+}
+
+
 function roll() {
+    document.getElementById('rollbutton').removeAttribute("onclick");
     const randomNummer = rollerArray[Math.floor(Math.random() * rollerArray.length)];
     element = document.querySelectorAll(".container div");
-    element2 = document.querySelector("#roller p");
-    element2.innerHTML = randomNummer;
-    element[randomNummer - 1].style.backgroundColor = "rgb(255, 228, 51)";
-    element[randomNummer - 1].innerHTML = randomNummer;
+    element2 = document.querySelectorAll("#roller ul li");
+    element3 = document.querySelector("#roller ul");
+    element2[9].innerHTML = randomNummer;
+    for (let i = 0; i < 9; i++) {
+        const randomNummers = Math.floor(Math.random() * 75) + 1;
+        element2[i].innerHTML = randomNummers;
+    }
+    element3.classList.remove("rollnumbers");
+    element3.classList.add("rollanimation");
+    element3.style.animation = 'none';
+    element3.offsetHeight; /* trigger reflow */
+    element3.style.animation = null;
+    const myTimeout = setTimeout(revealNumber, 5000);
+
+    function revealNumber() {
+        element[randomNummer - 1].style.background = "linear-gradient(180deg, rgba(251,247,95,1) 49%, rgba(200,184,0,1) 100%)";
+        element[randomNummer - 1].innerHTML = randomNummer;
+        element4 = document.querySelectorAll("#roller .rollanimation");
+        document.getElementById('rollbutton').setAttribute('onclick', 'roll()')
+    }
     var index = rollerArray.indexOf(randomNummer);
     rollerArray.splice(index, 1);
     rolled.push(randomNummer);
@@ -180,7 +316,8 @@ function roll() {
     document.getElementById("nobingo").style.display = "none";
 }
 
-function bingoKnop() {
+document.getElementById("bingobutton").addEventListener("click", bingo);
+function bingo() {
     console.log(valseBingo)
     const containsAllB = rowB.every(element => {
         return rolled.includes(element);
@@ -261,15 +398,22 @@ function bingoKnop() {
     if (valseBingo.length == 0) {
         document.getElementById("nobingo").style.display = "inline-block";
         console.log("valsbingo1");
-    }
-    else {
+    } else {
         document.getElementById("welbingo").style.display = "inline-block";
     }
 }
 
 //drag and drop
-const stamp = document.querySelector(".stamp");
-stamp.addEventListener('dragstart', dragStart);
+
+
+
+
+
+
+
+
+
+/* stamp.addEventListener('dragstart', dragStart);
 stamp.addEventListener('dragend', dragEnd);
 
 //drag functions
@@ -317,52 +461,4 @@ for (const box of boxes) {
     box.addEventListener('dragenter', dragEnter);
     box.addEventListener('dragleave', dragLeave);
     box.addEventListener('drop', dragDrop);
-}
-
-
-
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-/* de commando's */
-var commandos = [ 'Bingo', 'bingo'];
-var grammar = '#JSGF V1.0; grammar commandos; public <commando> = ' + commandos.join(' | ') + ' ;'
-
-/* het luisterobject */
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
-
-/* als er een commando uitgesproken is */
-function spraakAfhandelen(event) {
-  let last = event.results.length - 1;
-  let commando = event.results[last][0].transcript;
-  console.log('Result received: ' + commando + '. ' + 'Confidence: ' + event.results[0][0].confidence);
-
-  if ( commando.trim() == "bingo" || commando.trim() == "Bingo") {
-    bingoKnop()
-  }
-}
-
-function luisteren(){
-   recognition.start();
-   console.log('Ready to receive a command.');
-}
-
-/* het luisterobject de commando's leren */
-speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
-recognition.continuous = true;
-recognition.lang = 'nl';
-recognition.interimResults = true;
-recognition.maxAlternatives = 1;
-
-recognition.onresult = function(event) {
-   spraakAfhandelen(event);
-}
-
-recognition.onend = function() {
-   luisteren();
-}
-
-luisteren();
+} */
